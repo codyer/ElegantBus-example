45c75ebed6e3613f15096e700fda9d993bf41cce
codyer <kadierxu@sina.com>
2020-06-18 23:28:04 +0800

    测试新的ipc messenger 方式

--- app/src/main/java/com/example/bus/BusApplication.java
+++ app/src/main/java/com/example/bus/BusApplication.java
@@ -2,6 +2,7 @@ package com.example.bus;
 
 import android.app.Application;
 
+import cody.bus.ElegantBus;
 import cody.bus.ElegantBusX;
 
 
@@ -10,7 +11,9 @@ public class BusApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        ElegantBusX.supportMultiAppMultiProcess(this, "com.example.bus");
+        ElegantBus.setDebug(true);
+//        ElegantBusX.supportMultiAppMultiProcess(this, "com.example.bus");
+        ElegantBusX.supportSingleAppMultiProcess(this);
     }
 
     @Override
--- app/src/main/java/com/example/bus/TestUtil.java
+++ app/src/main/java/com/example/bus/TestUtil.java
@@ -10,25 +10,25 @@ import com.example.bus.cody.TestScopeBus;
 import androidx.lifecycle.LifecycleOwner;
 import cody.bus.ObserverWrapper;
 
-public class TestUtil {
+class TestUtil {
     private static String Normal = "Normal";
     private static String Sticky = "Sticky";
     private static String Forever = "Forever";
     private static int count = 0;
 
-    public static void postInt() {
+    static void postInt() {
         TestScopeBus.eventInt().post((10 + count++));
     }
 
-    public static void postString() {
+    static void postString() {
         TestScopeBus.eventString().post("字符串" + (count++));
     }
 
-    public static void postBean() {
+    static void postBean() {
         TestScopeBus.eventBean().post(new JavaBean("测试", count++));
     }
 
-    public static ObserverWrapper<Integer> testInt(LifecycleOwner owner, String page) {
+    static ObserverWrapper<Integer> testInt(LifecycleOwner owner, String page) {
         TestScopeBus.eventInt().observe(owner, new ObserverWrapper<Integer>() {
             @Override
             public void onChanged(final Integer value) {
@@ -51,7 +51,7 @@ public class TestUtil {
         return observerWrapper;
     }
 
-    public static ObserverWrapper<String> testString(LifecycleOwner owner, String page) {
+    static ObserverWrapper<String> testString(LifecycleOwner owner, String page) {
         TestScopeBus.eventString().observe(owner, new ObserverWrapper<String>() {
             @Override
             public void onChanged(final String value) {
@@ -74,7 +74,7 @@ public class TestUtil {
         return observerWrapper;
     }
 
-    public static ObserverWrapper<JavaBean> testBean(LifecycleOwner owner, String page) {
+    static ObserverWrapper<JavaBean> testBean(LifecycleOwner owner, String page) {
         TestScopeBus.eventBean().observe(owner, new ObserverWrapper<JavaBean>() {
             @Override
             public void onChanged(final JavaBean value) {
@@ -97,24 +97,24 @@ public class TestUtil {
         return observerWrapper;
     }
 
-    public static void open(Context context, Class<?> clz) {
+    static void open(Context context, Class<?> clz) {
         Intent intent = new Intent(context, clz);
         context.startActivity(intent);
     }
 
-    public static void removeInt(ObserverWrapper<Integer> observerWrapper) {
+    static void removeInt(ObserverWrapper<Integer> observerWrapper) {
         TestScopeBus.eventInt().removeObserver(observerWrapper);
     }
 
-    public static void removeString(ObserverWrapper<String> observerWrapper) {
+    static void removeString(ObserverWrapper<String> observerWrapper) {
         TestScopeBus.eventString().removeObserver(observerWrapper);
     }
 
-    public static void removeBean(ObserverWrapper<JavaBean> observerWrapper) {
+    static void removeBean(ObserverWrapper<JavaBean> observerWrapper) {
         TestScopeBus.eventBean().removeObserver(observerWrapper);
     }
 
-    public static void log(String type, Object value, String page) {
-        Log.d("TestUtil", Application.getProcessName() + ":" + page + ":收到 " + type + " 消息(" + value.getClass().getCanonicalName() + "):" + value);
+    private static void log(String type, Object value, String page) {
+        Log.d("TestUtil_ElegantBus", Application.getProcessName() + ":" + page + ":收到 " + type + " 消息(" + value.getClass().getCanonicalName() + "):" + value);
     }
 }

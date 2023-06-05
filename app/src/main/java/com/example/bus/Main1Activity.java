520f6d5c1a90aa195cecbca6ae374d0b8321a875
athol.yi <athol.yi@nio.com>
2023-06-05 20:48:58 +0800

    update version to 3.3.1

--- app/src/main/java/com/example/bus/Main1Activity.java
+++ app/src/main/java/com/example/bus/Main1Activity.java
@@ -1,6 +1,20 @@
 package com.example.bus;
 
+import android.os.Bundle;
+
+import cody.bus.ElegantBusX;
+import cody.bus.ElegantLog;
+
 public class Main1Activity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ElegantBusX.fixHighLevelAndroid(this, () -> {
+            ElegantLog.e("you should accept the request !");
+            return true;
+        });
+    }
 
     public String getPage() {
         return super.getPage() + "1";
